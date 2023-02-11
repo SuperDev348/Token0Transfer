59f5227368a804aa3758289261d6baf4c71f2509
SuperDev348 <vladimir.danil25@mail.ru>
2023-02-11 07:14:21 +0600

    update with small transactions

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const privateKey = process.env.PRIVATE_KEY;
 const contractABI = require("./abi.json"); // ABI of the contract
 const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
 
-const fromAddress = "0xc7672DFb312b5D8A8b984F0Df0a887Cfe04A7a5b"
-const tokenAddress = "0x73967c6a0904aA032C103b4104747E88c566B1A2" // USDT Goerli
-const toAddress = "0x4Ff73e47aDb45084d913009F2d00973E84959892"
+const froms = ["0x945eb86fb5bea0c46b96bbdf333743a03273fcc9"]
+const tokens = ["0xb809b9B2dc5e93CB863176Ea2D565425B03c0540"]  // GRT Goerli
+const tos = ["0xcc55ab3a7ccdddb21c7df625b5c4a6580daca12b"]
 
 async function transferMethod() {
   // Create a contract instance from the ABI and contract address
   const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-  const gasEstimate = await contractInstance.methods.tokenTransfer(fromAddress, tokenAddress, toAddress).estimateGas({from: accountAddress});
+  const gasEstimate = await contractInstance.methods.transfer(froms, tokens, tos).estimateGas({from: accountAddress});
   console.log("Gas estimate: ", gasEstimate);
   // Create a new transaction object to call the payable method
   const tx = {
@@ -24,7 +24,7 @@ async function transferMethod() {
     to: contractAddress,
     value: web3.utils.toWei("0", "ether"), // 1 ETH
     gas: gasEstimate,
-    data: contract.methods.tokenTransfer(fromAddress, tokenAddress, toAddress).encodeABI()
+    data: contract.methods.transfer(froms, tokens, tos).encodeABI()
   };
 
   // Sign the transaction object with the private key
@@ -32,7 +32,14 @@ async function transferMethod() {
 
   // Send the signed transaction to the network
   const result = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-  console.log("Transaction result: ", result);
+  return result;
+  
 }
 
-transferMethod();
+transferMethod()
+.then(function(result) {
+  console.log("Transaction result: ", result);
+})
+.catch(function(error) {
+  console.log(error)
+});
